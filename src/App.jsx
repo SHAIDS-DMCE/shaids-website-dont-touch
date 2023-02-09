299077fd39638762a9d9b77b7b3e651d3fe54e3d
Yash Bhangale <68957369+yashbhangale@users.noreply.github.com>
2023-02-09 12:55:35 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import './animation.css'
+import './script.js'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    
+    <div id="app">
+    <div id="hero">
+      <h1>NEON<br/>CURSOR</h1>
+      <a target="_blank" href="https://github.com/klevron/threejs-toys">github/threejs-toys</a>
+    </div>
+  </div>
+  
+  )
+}
+
+export default App
